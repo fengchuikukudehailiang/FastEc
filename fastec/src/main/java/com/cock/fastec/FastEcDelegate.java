8539e924197c502f80d5830e5d81fdee3c01cb7b
Cock <fengchuikukudehailiang@gmail.com>
2018-06-10 17:15:41 +0800

    6.使用Retrofit封装网络请求框架

--- fastec/src/main/java/com/cock/fastec/FastEcDelegate.java
+++ fastec/src/main/java/com/cock/fastec/FastEcDelegate.java
@@ -4,8 +4,13 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.View;
+import android.widget.Toast;
 
 import com.cock.latte.core.delegates.LatteDelegate;
+import com.cock.latte.core.net.RestClient;
+import com.cock.latte.core.net.callback.IError;
+import com.cock.latte.core.net.callback.IFailure;
+import com.cock.latte.core.net.callback.ISuccess;
 
 public class FastEcDelegate extends LatteDelegate {
 
@@ -16,6 +21,32 @@ public class FastEcDelegate extends LatteDelegate {
 
     @Override
     public void onBindView(@Nullable Bundle savedInstanceState, @NonNull View rootView) {
+        testRestClient();
+    }
 
+    private void testRestClient() {
+        RestClient.builder()
+                .url("http://www.baidu.com")
+                .params("", "")
+                .success(new ISuccess() {
+                    @Override
+                    public void onSuccess(String response) {
+                        Toast.makeText(getContext(), response, Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .failure(new IFailure() {
+                    @Override
+                    public void onFailure() {
+
+                    }
+                })
+                .error(new IError() {
+                    @Override
+                    public void onError(int code, String msg) {
+
+                    }
+                })
+                .build()
+                .get();
     }
 }
---
+++ latte-core/src/main/java/com/cock/latte/core/net/RestCreator.java
@@ -0,0 +1,63 @@
+package com.cock.latte.core.net;
+
+import com.cock.latte.core.app.ConfigKeys;
+import com.cock.latte.core.app.Latte;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.WeakHashMap;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.converter.scalars.ScalarsConverterFactory;
+
+public final class RestCreator {
+
+    /**
+     * 构建全局Retrofit客户端
+     */
+    private static final class RetrofitHolder {
+        private static final String BASE_URL = Latte.getConfiguration(ConfigKeys.API_HOST);
+        private static final Retrofit RETROFIT_CLIENT = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .client(OKHttpHolder.OK_HTTP_CLIENT)
+                .addConverterFactory(ScalarsConverterFactory.create())
+                .build();
+    }
+
+    /**
+     * 构建OkHttp
+     */
+    private static final class OKHttpHolder {
+        private static final int TIME_OUT = 60;
+
+        public static final OkHttpClient OK_HTTP_CLIENT = new OkHttpClient.Builder()
+                .connectTimeout(TIME_OUT, TimeUnit.SECONDS)
+                .build();
+    }
+
+    /**
+     * 参数容器
+     */
+    private static final class ParamsHolder {
+        private static final WeakHashMap<String, Object> PARAMS = new WeakHashMap<>();
+    }
+
+    public static WeakHashMap<String, Object> getParams() {
+        return ParamsHolder.PARAMS;
+    }
+
+    /**
+     * Service接口
+     */
+    private static final class RestServiceHolder {
+        private static final RestService REST_SERVICE =
+                RetrofitHolder.RETROFIT_CLIENT.create(RestService.class);
+    }
+
+    public static RestService getRestService() {
+        return RestServiceHolder.REST_SERVICE;
+    }
+
+}
